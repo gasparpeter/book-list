f261a48f130f511c5fb622857ca3363e1ed5b7d7
Peter Gaspar <petihappygaspar@gmail.com>
2019-06-30 21:19:56 +0300

    - fixed problems in book form

--- booklist.js
+++ booklist.js
@@ -1,5 +1,5 @@
 // Book Class : Represents a Book
-class book {
+class Book {
   constructor(title, author, isbn) {
       this.title = title;
       this.author = author;
@@ -46,11 +46,25 @@ class UI {
     }
 }
 
+
 // Store Class: Handles Storage
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a book
+document.querySelector('#book-form').addEventListener('submit', (e) =>
+{
+        e.preventDefault();
+
+        const title = document.querySelector('#title').value;
+        const author = document.querySelector('#author').value;
+        const isbn = document.querySelector('#isbn').value;
+
+
+        const book = new Book(title, author, isbn);
+
+        console.log(book);
+});
 
 // Event: Remove a Book
