0d1c7d1b96ed84fe321437cf2656d29214ab49df
Peter Gaspar <petihappygaspar@gmail.com>
2019-06-30 20:25:13 +0300

    - created the elements in html, javascript

---
+++ booklist.js
@@ -0,0 +1,56 @@
+// Book Class : Represents a Book
+class book {
+  constructor(title, author, isbn) {
+      this.title = title;
+      this.author = author;
+      this.isbn = isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '3433443'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane Doe',
+                isbn: '45545'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookTolist(book));
+    }
+
+    static addBookTolist(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td> 
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm 
+            delete">X</a></td>       
+        `;
+
+        list.appendChild(row);
+    }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a book
+
+// Event: Remove a Book
