69138e48d91fb68cef878cbe9f21cac060e3f559
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-02 17:53:43 +0300

    - show alert wanish in 3 second

--- booklist.js
+++ booklist.js
@@ -60,6 +60,8 @@ class UI {
         const container = document.querySelector('.container');
         const form = document.querySelector('#book-form');
         container.insertBefore(div, form);
+
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
     }
 
 
